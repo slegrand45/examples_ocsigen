d4988dfa78f7653173e9b68681ca1ba283a162fb
Stephane Legrand <slegrand45@gmail.com>
2016-06-11 14:38:31 +0200

    Remove useless packages

--- jsoo/curriculum-vitae/build.sh
+++ jsoo/curriculum-vitae/build.sh
@@ -1,6 +1,6 @@
 
 ocamlbuild -use-ocamlfind \
-  -pkgs lwt.ppx,js_of_ocaml,js_of_ocaml.ppx,js_of_ocaml.tyxml,tyxml,ppx_deriving,js_of_ocaml.deriving.ppx,js_of_ocaml.deriving,react,reactiveData \
+  -pkgs js_of_ocaml,js_of_ocaml.ppx,js_of_ocaml.tyxml,tyxml,react,reactiveData \
   main.byte ;
 
 js_of_ocaml +weak.js --opt 3 -o js/main.js main.byte
