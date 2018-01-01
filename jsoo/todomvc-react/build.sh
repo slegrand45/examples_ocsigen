2bfbcc67841772147ea1b48c261f41bfa0d5dc64
Stephane Legrand <slegrand45@gmail.com>
2018-01-01 16:21:52 +0100

    Update names of packages

--- jsoo/todomvc-react/build.sh
+++ jsoo/todomvc-react/build.sh
@@ -2,7 +2,7 @@
 
 # Compile OCaml source file to OCaml bytecode
 ocamlbuild -use-ocamlfind \
-  -pkgs lwt.ppx,js_of_ocaml,js_of_ocaml.ppx,js_of_ocaml.tyxml,tyxml,ppx_deriving,js_of_ocaml.deriving.ppx,js_of_ocaml.deriving,react,reactiveData \
+  -pkgs lwt.ppx,js_of_ocaml-lwt,js_of_ocaml.ppx,js_of_ocaml.tyxml,tyxml,ppx_deriving,js_of_ocaml-ppx.deriving,js_of_ocaml.deriving,react,reactiveData \
   todomvc.byte ;
 
 # Build JS code from the OCaml bytecode
