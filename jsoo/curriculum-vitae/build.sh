e2f5efe57caf7a644795ac6b14f6d6e04168e4be
Stephane Legrand <slegrand45@gmail.com>
2020-05-01 00:20:31 +0200

    Rename packages

--- jsoo/curriculum-vitae/build.sh
+++ jsoo/curriculum-vitae/build.sh
@@ -1,6 +1,6 @@
 
 ocamlbuild -use-ocamlfind \
-  -pkgs js_of_ocaml-lwt,js_of_ocaml.ppx,js_of_ocaml.tyxml,tyxml,react,reactiveData \
+  -pkgs js_of_ocaml-lwt,js_of_ocaml-ppx,js_of_ocaml-tyxml,tyxml,react,reactiveData \
   main.byte ;
 
 js_of_ocaml --opt 3 -o js/main.js main.byte
